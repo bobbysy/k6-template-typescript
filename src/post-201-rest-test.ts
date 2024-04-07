57cf7c144ae68f5276b36a7edd1fd84ca3071321
bobbysy <25177772+bobbysy@users.noreply.github.com>
2024-04-08 00:39:27 +0800

    feat: file upload with rest test

---
+++ src/post-201-rest-test.ts
@@ -0,0 +1,43 @@
+import { check, sleep } from "k6";
+import http from "k6/http";
+import type { Options } from "k6/options";
+import { FormData } from "https://jslib.k6.io/formdata/0.0.2/index.js";
+
+export const options: Options = {
+  discardResponseBodies: true,
+  scenarios: {
+    restUpload: {
+      executor: "constant-vus",
+      exec: "restUpload",
+      vus: 10,
+      duration: "10s",
+      gracefulStop: "0s", // do not wait for iterations to finish in the end
+    },
+  },
+};
+
+export const binFile = open("/path/to/files/foo.pdf", "b");
+
+export var uploaded = false;
+
+export function restUpload() {
+  // override to execute the API request exactly once
+  if (!uploaded && __ITER === 0) {
+    const fd = new FormData();
+    fd.append("file", {
+      data: new Uint8Array(binFile).buffer,
+      filename: "foo.pdf",
+      content_type: "application/json",
+    });
+    const res = http.post("http://localhost:8000/upload", fd.body(), {
+      headers: {
+        "Content-Type": "multipart/form-data; boundary=" + fd.boundary,
+      },
+    });
+    check(res, {
+      "status is 201": () => res.status === 201,
+    });
+  }
+  sleep(3);
+  uploaded = true;
+}
